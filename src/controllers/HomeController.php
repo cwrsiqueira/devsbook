6326a72c2752761ced388f9f6cf0a0d82327fa4a
cwrsiqueira <cwrsiqueira@msn.com>
2022-01-11 04:45:12 -0300

    first commit

---
+++ src/controllers/HomeController.php
@@ -0,0 +1,22 @@
+<?php
+namespace src\controllers;
+
+use \core\Controller;
+use \src\Handlers\LoginHandler;
+
+class HomeController extends Controller {
+
+    private $loggedUser;
+
+    public function __construct() {
+        $this->loggedUser = LoginHandler::checkLogin();
+        if($this->loggedUser === false) {
+            $this->redirect('/login');
+        }
+    }
+
+    public function index() {
+        $this->render('home', ['nome' => 'Bonieky']);
+    }
+
+}
